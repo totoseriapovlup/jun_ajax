7eee084f0bd315570d19396fe17fb0dc3b7f00b0
toto <test@test.com>
2023-12-25 21:04:38 +0200

    controller add Api controller

---
+++ app/controllers/Api.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace app\controllers;
+
+
+class Api implements \app\core\controllerable
+{
+
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+
+    public function store()
+    {
+        var_dump($_POST);
+    }
+}
