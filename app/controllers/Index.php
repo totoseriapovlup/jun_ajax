515cca029e6ab60302637becfcd69cd462b5e91c
toto <test@test.com>
2023-12-25 20:12:07 +0200

    controller Index

---
+++ app/controllers/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace app\controllers;
+
+
+use app\core\controllerable;
+
+class Index implements controllerable
+{
+    /**
+     * index action
+     */
+    public function index()
+    {
+        echo 'index index';
+    }
+
+}
