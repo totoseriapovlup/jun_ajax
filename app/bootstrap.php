2c4d6b6484e8bf73425af41e48363195531891aa
toto <test@test.com>
2023-12-25 19:16:00 +0200

    config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,8 @@
 
 include_once '../app/core/App.php';
 
+include_once \app\core\App::getRootPath() . 'app' . DIRECTORY_SEPARATOR . 'config.php';
+
 spl_autoload_register(function ($class) {
     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     $path = \app\core\App::getRootPath() . $class . '.php';
---
+++ app/config.example.php
@@ -0,0 +1,7 @@
+<?php
+
+#DB
+const DB_HOST = 'localhost';
+const DB_USER = '';
+const DB_PASS = '';
+const DB_NAME = 'jun_ajax';
