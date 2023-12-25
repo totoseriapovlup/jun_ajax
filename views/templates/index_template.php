586ff38d1516fb06cc4b758d0365d978bbf5e004
toto <test@test.com>
2023-12-25 20:26:43 +0200

    view index_template

---
+++ views/templates/index_template.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div>Ajax site example</div>
+    </header>
+    <main>
+        <?php include_once $this->viewsPath() . 'pages' . DIRECTORY_SEPARATOR . $this->page . '_view.php';?>
+    </main>
+    <footer>
+        <div>php 23-2</div>
+    </footer>
+</body>
+</html>
