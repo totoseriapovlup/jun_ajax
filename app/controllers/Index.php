777e7382584cd2ecf6005843f465d47c3d98d6ea
toto <test@test.com>
2023-12-25 20:27:11 +0200

    controller index add index action

--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -5,15 +5,29 @@ namespace app\controllers;
 
 
 use app\core\controllerable;
+use app\core\View;
 
 class Index implements controllerable
 {
+    /**
+     * @var View
+     */
+    protected View $view;
+
+    /**
+     * Index constructor.
+     */
+    public function __construct()
+    {
+        $this->view = new View();
+    }
+
     /**
      * index action
      */
     public function index()
     {
-        echo 'index index';
+        $this->view->render('index');
     }
 
 }
