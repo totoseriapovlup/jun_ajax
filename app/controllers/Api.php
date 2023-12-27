c2b660eb87403fe3eb3df8950ff8a031c0d22dd5
toto <test@test.com>
2023-12-27 19:25:31 +0200

    controller Api add store of new note

--- app/controllers/Api.php
+++ app/controllers/Api.php
@@ -4,16 +4,41 @@
 namespace app\controllers;
 
 
+use app\models\Note;
+
 class Api implements \app\core\controllerable
 {
+    /**
+     * @var Note
+     */
+    protected Note $model;
 
+    /**
+     * Api constructor.
+     */
+    public function __construct()
+    {
+        $this->model = new Note();
+    }
+
+    /**
+     * get all notes
+     */
     public function index()
     {
         // TODO: Implement index() method.
     }
 
+    /**
+     * store new note
+     * @throws \app\exceptions\BadQueryException
+     */
     public function store()
     {
-        var_dump($_POST);
+        $note = [];
+        $note['title'] = htmlspecialchars(filter_input(INPUT_POST, 'note'));
+        //TODO validate
+        $this->model->add($note);
+        http_response_code(201);
     }
 }
