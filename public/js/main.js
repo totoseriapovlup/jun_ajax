416686b98fc86cbe14f6cc5f8f465a1c8a50ef11
toto <test@test.com>
2023-12-27 19:25:56 +0200

    js create note form submit

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,18 @@
 document.forms.create.onsubmit = function (e) {
+    let me = this;                      //for using this form in callbacks
     let data = new FormData(this);
     let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                alert('created');
-            } else {
-                alert('some error');
-            }
+    xhr.onload = function () {
+        if (xhr.status === 201) {
+            alert('Note created');
+            me.reset();
         }
+    };
+    xhr.onerror = function () {
+        alert('some error');
+        console.error(xhr.status + xhr.text);
     };
     xhr.open(this.method, this.action);
     xhr.send(data);
-
     e.preventDefault();
 };
